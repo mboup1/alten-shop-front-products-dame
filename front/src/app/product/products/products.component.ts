70dd66c69880a4642a3dce68a5645e431e9a8b04
mboup <mboup124@gmail.com>
2024-07-10 21:25:55 +0200

    product list formatting

--- front/src/app/product/products/products.component.ts
+++ front/src/app/product/products/products.component.ts
@@ -24,4 +24,13 @@ export class ProductsComponent implements OnInit {
     console.log('Adding item to cart:', product);
   }
 
+  generateStarRating(rating: number) {
+    const maxStars = 5;
+    const fullStars = Math.floor(rating);
+    const halfStar = rating % 1 !== 0;
+    const emptyStars = maxStars - fullStars - (halfStar ? 1 : 0);
+    const stars = '★'.repeat(fullStars) + (halfStar ? '½' : '') + '☆'.repeat(emptyStars);
+    return stars;
+  }
+
 }
